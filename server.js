5e05bdae29e95783ebf91e20aab73c171a63bcb2
Dangel2 <93921535+Dangel2@users.noreply.github.com>
2026-06-04 03:34:30 -0600

    Add files via upload

---
+++ server.js
@@ -0,0 +1,19 @@
+const express = require("express");
+const app = express();
+const http = require("http").createServer(app);
+const io = require("socket.io")(http);
+
+app.use(express.static("public"));
+
+io.on("connection", (socket) => {
+  socket.on("join-room", (room) => {
+    socket.join(room);
+  });
+
+  socket.on("video-sync", (data) => {
+    io.to(data.room).emit("video-sync", data);
+  });
+});
+
+const PORT = process.env.PORT || 3000;
+http.listen(PORT, () => console.log("Servidor listo"));
