fb115021e6e1b6bddc315e0903f87fd3ec4f4830
Dangel2 <93921535+Dangel2@users.noreply.github.com>
2026-06-04 09:05:11 -0600

    Actualizar script.js

--- public/script.js
+++ public/script.js
@@ -1,12 +1,15 @@
 const socket = io();
 const video = document.getElementById("video");
 
-// nombre de sala desde el link
 const room = window.location.pathname.split("/")[2] || "amor";
 
 socket.emit("join-room", room);
 
+let syncing = false;
+
 function send(action) {
+  if (syncing) return;
+
   socket.emit("video-sync", {
     room,
     action,
@@ -19,17 +22,26 @@ video.addEventListener("pause", () => send("pause"));
 video.addEventListener("seeked", () => send("seek"));
 
 socket.on("video-sync", (data) => {
+
+  syncing = true;
+
   if (data.action === "play") {
     video.currentTime = data.time;
-    video.play();
+
+    video.play().catch(() => {});
   }
 
   if (data.action === "pause") {
     video.currentTime = data.time;
+
     video.pause();
   }
 
   if (data.action === "seek") {
     video.currentTime = data.time;
   }
+
+  setTimeout(() => {
+    syncing = false;
+  }, 500);
 });
