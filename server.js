a7ddfa08b3ae120f44012f32731f0ef9a9a0719e
Dangel2 <93921535+Dangel2@users.noreply.github.com>
2026-06-04 23:30:19 -0600

    Actualizar server.js

--- server.js
+++ server.js
@@ -3,27 +3,37 @@ const app = express();
 const http = require("http").createServer(app);
 const io = require("socket.io")(http);
 
+let currentVideo = "";
+
 app.use(express.static("public"));
 
 io.on("connection", (socket) => {
 
   socket.on("join-room", (room) => {
+
     socket.join(room);
+
+    if (currentVideo) {
+      socket.emit("video-change", {
+        url: currentVideo
+      });
+    }
+
   });
 
   socket.on("video-sync", (data) => {
 
-    // enviar a todos MENOS al que originó el evento
     socket.to(data.room).emit("video-sync", data);
 
   });
-  
+
   socket.on("video-change", (data) => {
 
-  socket.to(data.room)
-        .emit("video-change", data);
+    currentVideo = data.url;
 
-});
+    socket.to(data.room).emit("video-change", data);
+
+  });
 
 });
 
