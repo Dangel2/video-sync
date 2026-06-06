9994a03d77a4b5d7daa6eb0b6844d82e69bdea36
Dangel2 <93921535+Dangel2@users.noreply.github.com>
2026-06-06 17:09:16 -0600

    Actualizar script.js

--- public/script.js
+++ public/script.js
@@ -2,21 +2,19 @@ const socket = io();
 const video = document.getElementById("video");
 
 const room = window.location.pathname.split("/")[2] || "amor";
-
 const isHost = window.location.search.includes("clave=nestor123");
 
-if (isHost) {
-  document.getElementById("adminPanel").style.display = "block";
-}
+const VIDEO_URL = "REEMPLAZA_AQUI_TU_LINK_MP4_DIRECTO";
+
+video.src = VIDEO_URL;
+video.load();
 
 socket.emit("join-room", room);
 
 let syncing = false;
 
 function send(action) {
-
   if (!isHost) return;
-
   if (syncing) return;
 
   socket.emit("video-sync", {
@@ -24,17 +22,12 @@ function send(action) {
     action,
     time: video.currentTime
   });
-
 }
 
 if (isHost) {
-
   video.addEventListener("play", () => send("play"));
-
   video.addEventListener("pause", () => send("pause"));
-
   video.addEventListener("seeked", () => send("seek"));
-
 }
 
 if (!isHost) {
@@ -42,58 +35,23 @@ if (!isHost) {
 }
 
 socket.on("video-sync", (data) => {
-
   syncing = true;
 
   if (data.action === "play") {
-
     video.currentTime = data.time;
-
     video.play().catch(() => {});
-
   }
 
   if (data.action === "pause") {
-
     video.currentTime = data.time;
-
     video.pause();
-
   }
 
   if (data.action === "seek") {
-
     video.currentTime = data.time;
-
   }
 
   setTimeout(() => {
     syncing = false;
   }, 500);
-
 });
-
-socket.on("video-change", (data) => {
-
-  video.src = data.url;
-
-  video.load();
-
-});
-
-function cambiarVideo() {
-
-  const url = document.getElementById("videoUrl").value;
-
-  if (!url) return;
-
-  video.src = url;
-
-  video.load();
-
-  socket.emit("video-change", {
-    room,
-    url
-  });
-
-}
