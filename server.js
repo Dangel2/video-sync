fc907a035d5201254e4b26edf635259df6174937
Dangel2 <93921535+Dangel2@users.noreply.github.com>
2026-06-07 17:23:46 -0600

    Update server.js

--- server.js
+++ server.js
@@ -12,21 +12,18 @@ let rooms = {};
 
 io.on('connection', (socket) => {
 
-  socket.on('join-room', ({ room, username }) => {
+  socket.on('join-room', ({ room, username, isHost }) => {
     socket.join(room);
     socket.username = username;
+    socket.isHost = isHost;
 
     if (!rooms[room]) {
-      rooms[room] = {
-        playlist: [],
-        currentIndex: 0
-      };
+      rooms[room] = { playlist: [], currentIndex: 0 };
     }
 
     io.to(room).emit('playlist-update', rooms[room]);
   });
 
-  // ✅ CHAT ARREGLADO
   socket.on('chat-message', ({ room, message }) => {
     io.to(room).emit('chat-message', {
       username: socket.username,
@@ -35,17 +32,29 @@ io.on('connection', (socket) => {
   });
 
   socket.on('add-video', ({ room, video }) => {
+    if (!socket.isHost) return;
     rooms[room].playlist.push(video);
-
     io.to(room).emit('playlist-update', rooms[room]);
   });
 
   socket.on('select-video', ({ room, index }) => {
+    if (!socket.isHost) return;
     rooms[room].currentIndex = index;
-
     io.to(room).emit('video-selected', { index });
+  });
+
+  socket.on('remove-video', ({ room, index }) => {
+    if (!socket.isHost) return;
+
+    rooms[room].playlist.splice(index, 1);
+
+    if (rooms[room].currentIndex >= index) {
+      rooms[room].currentIndex = Math.max(0, rooms[room].currentIndex - 1);
+    }
+
+    io.to(room).emit('playlist-update', rooms[room]);
   });
 
 });
 
-server.listen(3000, () => console.log("Server running on port 3000"));
+server.listen(3000, () => console.log("Server running"));
