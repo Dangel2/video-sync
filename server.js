1653c514304e9447a9f5b44cc3085b7932e69be7
Dangel2 <93921535+Dangel2@users.noreply.github.com>
2026-06-07 18:00:03 -0600

    Actualizar server.js

--- server.js
+++ server.js
@@ -1,60 +1,25 @@
-const express = require('express');
-const http = require('http');
-const { Server } = require('socket.io');
 
-const app = express();
-const server = http.createServer(app);
-const io = new Server(server);
-
+const express=require('express');
+const http=require('http');
+const {Server}=require('socket.io');
+const app=express(); const server=http.createServer(app);
+const io=new Server(server);
 app.use(express.static('public'));
+let rooms={};
 
-let rooms = {};
-
-io.on('connection', (socket) => {
-
-  socket.on('join-room', ({ room, username, isHost }) => {
-    socket.join(room);
-    socket.username = username;
-    socket.isHost = isHost;
-
-    if (!rooms[room]) {
-      rooms[room] = { playlist: [], currentIndex: 0 };
-    }
-
-    io.to(room).emit('playlist-update', rooms[room]);
-  });
-
-  socket.on('chat-message', ({ room, message }) => {
-    io.to(room).emit('chat-message', {
-      username: socket.username,
-      message
-    });
-  });
-
-  socket.on('add-video', ({ room, video }) => {
-    if (!socket.isHost) return;
-    rooms[room].playlist.push(video);
-    io.to(room).emit('playlist-update', rooms[room]);
-  });
-
-  socket.on('select-video', ({ room, index }) => {
-    if (!socket.isHost) return;
-    rooms[room].currentIndex = index;
-    io.to(room).emit('video-selected', { index });
-  });
-
-  socket.on('remove-video', ({ room, index }) => {
-    if (!socket.isHost) return;
-
-    rooms[room].playlist.splice(index, 1);
-
-    if (rooms[room].currentIndex >= index) {
-      rooms[room].currentIndex = Math.max(0, rooms[room].currentIndex - 1);
-    }
-
-    io.to(room).emit('playlist-update', rooms[room]);
-  });
-
+io.on('connection',(socket)=>{
+ socket.on('join-room',({room,username,isHost})=>{
+  socket.join(room); socket.username=username; socket.isHost=isHost;
+  if(!rooms[room]) rooms[room]={playlist:[],currentIndex:0,playback:{playing:false,time:0}};
+  socket.emit('room-state',rooms[room]);
+  io.to(room).emit('playlist-update',rooms[room]);
+ });
+ socket.on('chat-message',({room,message})=>{
+  io.to(room).emit('chat-message',{username:socket.username,message,senderId:socket.id});
+ });
+ socket.on('add-video',({room,video})=>{ if(!socket.isHost)return; rooms[room].playlist.push(video); io.to(room).emit('playlist-update',rooms[room]);});
+ socket.on('select-video',({room,index})=>{ if(!socket.isHost)return; rooms[room].currentIndex=index; io.to(room).emit('video-selected',{index});});
+ socket.on('remove-video',({room,index})=>{ if(!socket.isHost)return; rooms[room].playlist.splice(index,1); io.to(room).emit('playlist-update',rooms[room]);});
+ socket.on('sync-state',({room,state})=>{ if(!socket.isHost)return; rooms[room].playback=state; socket.to(room).emit('sync-state',state);});
 });
-
-server.listen(3000, () => console.log("Server running"));
+server.listen(3000);
